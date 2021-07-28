4863425628c0d4f424745dd5a4a5319fe322921a
karina contreras <kevinja21@gmail.com>
2021-07-28 18:23:04 -0300

    avanzando

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,21 +4,31 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new 
   end
 
   def create
+    @task = Task.new(params[:task])
+    @task.save
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to taks_path
   end
 
 end
